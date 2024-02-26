94d2a2dddaef300694b33d959e0ac0bed300ad1f
leo <leoasrgl@gmail.com>
2024-02-26 14:53:44 -0300

    first commit

---
+++ app/src/main/java/com/example/sendme/app/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sendme.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sendme.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
